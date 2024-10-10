2d1af503b665954f7b9174996e07150c07e358c7
张炎泼 <drdr.xp@gmail.com>
2024-10-10 11:19:50 +0800

    Refactor: fix import path

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
-pub mod checksum_reader;
-pub mod checksum_writer;
-pub mod codec;
-pub mod fixed_size;
+mod checksum_reader;
+mod checksum_writer;
+mod codec;
+mod fixed_size;
+mod with_checksum;
+
 pub mod testing;
-pub mod with_checksum;
+
+pub use checksum_reader::ChecksumReader;
+pub use checksum_writer::ChecksumWriter;
+pub use codec::Codec;
+pub use fixed_size::FixedSize;
+pub use with_checksum::WithChecksum;
