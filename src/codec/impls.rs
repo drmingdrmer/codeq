14a38b830cc08eb35abf3eb77b621af2d25ab3e1
张炎泼 <drdr.xp@gmail.com>
2024-10-11 10:33:16 +0800

    Feature: impl Codec for String and Vec<u8>

--- src/codec/impls.rs
+++ src/codec/impls.rs
@@ -1,5 +1,8 @@
 use std::io;
+use std::io::Error;
+use std::io::Read;
 
+use byteorder::BigEndian;
 use byteorder::ReadBytesExt;
 use byteorder::WriteBytesExt;
 
@@ -35,13 +38,51 @@ impl Decode for u32 {
     }
 }
 
+impl Encode for Vec<u8> {
+    fn encode<W: io::Write>(&self, mut w: W) -> Result<usize, io::Error> {
+        w.write_u32::<BigEndian>(self.len() as u32)?;
+        w.write_all(self)?;
+        Ok(self.len() + 4)
+    }
+}
+
+impl Decode for Vec<u8> {
+    fn decode<R: Read>(mut r: R) -> Result<Self, Error> {
+        let len = r.read_u32::<BigEndian>()? as usize;
+        let mut buf = vec![0; len];
+        r.read_exact(&mut buf)?;
+        Ok(buf)
+    }
+}
+
+impl Encode for String {
+    fn encode<W: io::Write>(&self, mut w: W) -> Result<usize, io::Error> {
+        let bytes = self.as_bytes();
+        w.write_u32::<BigEndian>(bytes.len() as u32)?;
+        w.write_all(bytes)?;
+        Ok(bytes.len() + 4)
+    }
+}
+
+impl Decode for String {
+    fn decode<R: io::Read>(mut r: R) -> Result<Self, io::Error> {
+        let len = r.read_u32::<BigEndian>()? as usize;
+        let mut buf = vec![0; len];
+        r.read_exact(&mut buf)?;
+        String::from_utf8(buf).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::fmt::Debug;
+    use std::io;
     use std::mem::size_of;
 
     use crate::codec::Codec;
     use crate::fixed_size::FixedSize;
+    use crate::Decode;
+    use crate::Encode;
 
     #[test]
     fn test_u64_codec() -> anyhow::Result<()> {
@@ -64,6 +105,34 @@ mod tests {
 
         let b = T::decode(&mut buf.as_slice())?;
         assert_eq!(v, b);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_vec_u8_codec() -> Result<(), io::Error> {
+        let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let mut buf = Vec::new();
+        let n = v.encode(&mut buf)?;
+        assert_eq!(n, buf.len());
+        assert_eq!(buf.len(), 4 + v.len());
+
+        let b = Vec::<u8>::decode(&mut buf.as_slice())?;
+        assert_eq!(v, b);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_string_codec() -> Result<(), io::Error> {
+        let s = "hello".to_string();
+        let mut buf = Vec::new();
+        let n = s.encode(&mut buf)?;
+        assert_eq!(n, buf.len());
+        assert_eq!(buf.len(), 4 + s.len());
+
+        let b = String::decode(&mut buf.as_slice())?;
+        assert_eq!(s, b);
 
         Ok(())
     }
