3c06c73f3286b660e5b75c84f7fceebc5203d6a5
张炎泼 <drdr.xp@gmail.com>
2024-10-11 10:38:17 +0800

    Feature: add codec for tuple (A, B)

--- src/codec/impls.rs
+++ src/codec/impls.rs
@@ -73,6 +73,23 @@ impl Decode for String {
     }
 }
 
+impl<A: Encode, B: Encode> Encode for (A, B) {
+    fn encode<W: io::Write>(&self, mut w: W) -> Result<usize, io::Error> {
+        let mut n = 0;
+        n += self.0.encode(&mut w)?;
+        n += self.1.encode(&mut w)?;
+        Ok(n)
+    }
+}
+
+impl<A: Decode, B: Decode> Decode for (A, B) {
+    fn decode<R: io::Read>(mut r: R) -> Result<Self, io::Error> {
+        let a = A::decode(&mut r)?;
+        let b = B::decode(&mut r)?;
+        Ok((a, b))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::fmt::Debug;
@@ -133,6 +150,20 @@ mod tests {
 
         let b = String::decode(&mut buf.as_slice())?;
         assert_eq!(s, b);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_tuple_2_codec() -> Result<(), io::Error> {
+        let v = (1u64, 2u32);
+        let mut buf = Vec::new();
+        let n = v.encode(&mut buf)?;
+        assert_eq!(n, buf.len());
+        assert_eq!(buf.len(), 8 + 4);
+
+        let b = <(u64, u32)>::decode(&mut buf.as_slice())?;
+        assert_eq!(v, b);
 
         Ok(())
     }
