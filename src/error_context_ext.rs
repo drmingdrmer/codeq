466c401aee145745dc9e98ae1d85b2a177eac22a
张炎泼 <drdr.xp@gmail.com>
2024-11-07 13:22:44 +0800

    Change: context receives a lambda instead of a ToString

--- src/error_context_ext.rs
+++ src/error_context_ext.rs
@@ -1,17 +1,18 @@
+use std::fmt::Display;
 use std::io;
 
 pub trait ErrorContextExt {
-    fn context(self, ctx: impl ToString) -> Self;
+    fn context<D: Display>(self, ctx: impl FnOnce() -> D) -> Self;
 }
 
 impl ErrorContextExt for io::Error {
-    fn context(self, ctx: impl ToString) -> Self {
-        io::Error::new(self.kind(), format!("{}; when:({})", self, ctx.to_string()))
+    fn context<D: Display>(self, ctx: impl FnOnce() -> D) -> Self {
+        io::Error::new(self.kind(), format!("{}; when:({})", self, ctx()))
     }
 }
 
 impl<T> ErrorContextExt for Result<T, io::Error> {
-    fn context(self, ctx: impl ToString) -> Self {
+    fn context<D: Display>(self, ctx: impl FnOnce() -> D) -> Self {
         self.map_err(|e| e.context(ctx))
     }
 }
