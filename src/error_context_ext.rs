433002dfef62dc1b0c6411c175b702bcc845085b
张炎泼 <drdr.xp@gmail.com>
2025-02-12 23:05:24 +0800

    Doc: improve doc for `ErrorContextExt`

--- src/error_context_ext.rs
+++ src/error_context_ext.rs
@@ -1,7 +1,35 @@
 use std::fmt::Display;
 use std::io;
 
+/// Trait for adding context to error messages.
+///
+/// This trait allows adding additional context to error messages by providing
+/// a closure that returns a displayable context string.
+///
+/// Example of adding context to an error:
+/// ```rust
+/// # use std::io;
+/// # use codeq::error_context_ext::ErrorContextExt;
+///
+/// let err = io::Error::new(io::ErrorKind::Other, "some error");
+/// let err = err.context(|| "some context");
+/// assert_eq!(err.to_string(), "some error; when:(some context)");
+/// ```
+///
+/// Example of adding context to a result:
+/// ```rust
+/// # use std::io;
+/// # use codeq::error_context_ext::ErrorContextExt;
+///
+/// let res = Result::<(), io::Error>::Err(io::Error::new(io::ErrorKind::Other, "some error"));
+/// let res = res.context(|| "some context");
+/// assert_eq!(res.unwrap_err().to_string(), "some error; when:(some context)");
+/// ```
 pub trait ErrorContextExt {
+    /// Adds context to the error message.
+    ///
+    /// # Arguments
+    /// * `ctx` - A closure that returns a displayable context string
     fn context<D: Display>(self, ctx: impl FnOnce() -> D) -> Self;
 }
 
