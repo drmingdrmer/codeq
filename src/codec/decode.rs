d710c59d1805de8207b077ed221dcf0870f722c6
张炎泼 <drdr.xp@gmail.com>
2025-02-12 23:15:52 +0800

    Doc: improve doc for `Decode`

--- src/codec/decode.rs
+++ src/codec/decode.rs
@@ -1,5 +1,18 @@
 use std::io;
 
+/// A trait for types that can be decoded from an [`io::Read`] stream.
+///
+/// Implementing this trait allows types to be decoded from an [`io::Read`] stream,
+/// which is useful for reading data from various sources like files, buffers, and streams.
+///
+/// # Examples
+/// ```rust
+/// use codeq::Decode;
+///
+/// let data = b"\x00\x00\x00\x05hello";
+/// let decoded = String::decode(&data[..]).unwrap();
+/// assert_eq!(decoded, "hello");
+/// ```
 pub trait Decode: Sized {
     fn decode<R: io::Read>(r: R) -> Result<Self, io::Error>;
 }
